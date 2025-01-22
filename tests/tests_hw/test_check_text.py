4b15bec6392d5444f11d0817cd3f1d08c9356c44
Vasiliy_Urazgaliev <Urazgaliev@list.ru>
2025-01-22 14:51:23 +0300

    demoqa

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -1,10 +1,16 @@
 from pages.demoqa import DemoQa
 from pages.elements_page import ElementsPage
 
-def test_text(browser):
+# Задание 1.
+def test_check_text_footer(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+# Задание 2.
+def test_check_text_elements_page(browser):
     demo_qa_page = DemoQa(browser)
     elements_page = ElementsPage(browser)
     demo_qa_page.visit()
     demo_qa_page.button.click()
-    assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
     assert elements_page.text_elements.get_text() == 'Please select an item from left to start practice.'
